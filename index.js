e27c887358aff9fdda752957808dd180265d8734
kentrino <kentrino@h-kento.jp>
2021-03-30 16:37:04 +0900

    add first event listener

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("test")
+addEventListener("fetch", (event) => {
+    const response = new Response("Hello World!", {
+      headers: { "content-type": "text/plain" },
+    });
+    event.respondWith(response);
+  });
